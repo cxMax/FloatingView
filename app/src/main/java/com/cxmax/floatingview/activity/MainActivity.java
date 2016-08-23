39591346830fb04e342c87984027106ec16ecc72
cxMax <caixi169@hotmail.com>
2016-08-24 00:06:57 +0800

    '使用ViewDragHelper实现拖拽广告，暂时不支持滑动显示隐藏悬浮广告的动效，打算使用观察者模式实现拖拽后重绘显示隐藏的效果'

--- app/src/main/java/com/cxmax/floatingview/activity/MainActivity.java
+++ app/src/main/java/com/cxmax/floatingview/activity/MainActivity.java
@@ -2,56 +2,59 @@ package com.cxmax.floatingview.activity;
 
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.cxmax.floatingview.R;
+import com.cxmax.floatingview.recyclerview.DividerItemDecoration;
+import com.cxmax.floatingview.recyclerview.RecyclerAdapter;
 import com.cxmax.library.widget.FloatingView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements FloatingView.OnFloatClickListener{
-//    private RecyclerView mRecyclerView;
-//    private List<String> mDatas;
-//    private FloatingView mFloatingView;
-    private List<View> mViews;
-    private ViewPager mViewPager;
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
     private FloatingView mFloatingView;
+//    private List<View> mViews;
+//    private ViewPager mViewPager;
+//    private FloatingView mFloatingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viewpager);
+        setContentView(R.layout.activity_main);
         initFloatView();
-        initData();
-        initView();
 //        initData();
-//        initRecyclerView();
 //        initView();
+        initData();
+        initRecyclerView();
+        initView();
     }
     private void initFloatView() {
         mFloatingView = (FloatingView) findViewById(R.id.float_view);
         mFloatingView.setOnFloatClickListener(this);
     }
 
-    private void initView() {
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new ViewPagerAdapter(mViews));
-        mFloatingView.attachToViewPager(mViewPager);
-    }
-
-    private void initData() {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        mViews = new ArrayList<View>();
-        mViews.add(inflater.inflate(R.layout.item_viewpager_one,null));
-        mViews.add(inflater.inflate(R.layout.item_viewpager_two,null));
-        mViews.add(inflater.inflate(R.layout.item_viewpager_three,null));
-    }
+//    private void initView() {
+//        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+//        mViewPager.setAdapter(new ViewPagerAdapter(mViews));
+//        mFloatingView.attachToViewPager(mViewPager);
+//    }
+//
+//    private void initData() {
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//        mViews = new ArrayList<View>();
+//        mViews.add(inflater.inflate(R.layout.item_viewpager_one,null));
+//        mViews.add(inflater.inflate(R.layout.item_viewpager_two,null));
+//        mViews.add(inflater.inflate(R.layout.item_viewpager_three,null));
+//    }
 
     @Override
     public void floatClick(View view) {
@@ -92,33 +95,29 @@ public class MainActivity extends AppCompatActivity implements FloatingView.OnFl
             return view == object;
         }
     }
-//    private void initRecyclerView() {
-//        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-//        mRecyclerView.setHasFixedSize(true);
-//        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-//        mRecyclerView.setAdapter(new RecyclerAdapter(mDatas,this));
-//    }
-//
-//    /**
-//     * 初始化数据
-//     */
-//    private void initData() {
-//        mDatas = new ArrayList<>();
-//        for (int i = 'A'; i < 'z'; i++)
-//        {
-//            mDatas.add("这是" + (char) i + "项");
-//        }
-//    }
-//    private void initView() {
-//        mFloatingView = (FloatingView) findViewById(R.id.float_view);
-//        mFloatingView.setOnFloatClickListener(this);
-//        mFloatingView.attachToRecyclerView(mRecyclerView);
-//    }
-//
-//    @Override
-//    public void floatClick(View view) {
-//        Toast.makeText(this,"hhhhhhh",Toast.LENGTH_SHORT).show();
-//    }
+    private void initRecyclerView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+        mRecyclerView.setAdapter(new RecyclerAdapter(mDatas,this));
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("这是" + (char) i + "项");
+        }
+    }
+    private void initView() {
+        mFloatingView = (FloatingView) findViewById(R.id.float_view);
+        mFloatingView.setOnFloatClickListener(this);
+        mFloatingView.attachToRecyclerView(mRecyclerView);
+    }
+
 }
